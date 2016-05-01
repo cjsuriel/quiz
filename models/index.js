9ccb011d5d3dcd1122447f61fe0816d1bde11255
cjsuriel <cj.suriel@alumnos.upm.es>
2016-05-01 19:40:53 +0200

    Tema10: Lista de preguntas

--- models/index.js
+++ models/index.js
@@ -2,39 +2,25 @@ var path = require('path');
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
+// var sequelize =new Sequelize (null, null, null,
+//			{dialect: "sqlite", storage: "quiz.squlite"});
+
+//Usar BBDD SQLite:
+//	DATABASE_URL = sqlite:///
+//	DATABASE_STORAGE = quiz.sqlite
+// Usar BBDD Postgress:
+// DATABASE_URL = postgres://user:passwd@host:port/database
 
 
-// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
-// SQLite   DATABASE_URL = sqlite://:@:/
-//var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var url = process.env.DATABASE_URL;
 var storage = process.env.DATABASE_STORAGE || "";
 
-//var DATABASE_PROTOCOL = url[1];
-//var DATABASE_DIALECT  = url[1];
-//var DATABASE_USER     = url[2];
-//var DATABASE_PASSWORD = url[3];
-//var DATABASE_HOST     = url[4];
-//var DATABASE_PORT     = url[5];
-//var DATABASE_NAME     = url[6];
 
 //var DATABASE_STORAGE  = process.env.DATABASE_STORAGE;
 var sequelize = new Sequelize(url,
 				{ storage: storage,
 				  omitNull: true
 				});
-
-// Usar BBDD SQLite o Postgres
-/*var sequelize = new Sequelize(DATABASE_NAME, 
-							  DATABASE_USER, 
-							  DATABASE_PASSWORD, 
-				              { dialect:  DATABASE_DIALECT, 
-				                protocol: DATABASE_PROTOCOL, 
-				                port:     DATABASE_PORT,
-				                host:     DATABASE_HOST,
-				                storage:  DATABASE_STORAGE,   // solo local (.env)
-				                omitNull: true                // solo Postgres
-				              });*/
 
 
 // Importar la definicion de la tabla Quiz de quiz.js
@@ -45,12 +31,12 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 sequelize.sync()
     .then(function() {
         // Ya se han creado las tablas necesarias.
-        return Quiz.count()
+          return Quiz.count()
                 .then(function (c) {
                     if (c === 0) {   // la tabla se inicializa solo si está vacía
-                        return Quiz.create({ question: 'Capital de Italia',
-          	                                 answer: 'Roma'
-          	                               })
+                        return Quiz.bulkCreate([ {question: 'Capital decvcvc Italia',   answer: 'Roma'},
+                                                 {question: 'Capital de Portugal', answer: 'Lisboa'}
+                                              ])
                                    .then(function() {
                                         console.log('Base de datos inicializada con datos');
                                     });
@@ -62,5 +48,5 @@ sequelize.sync()
         process.exit(1);
     });
 
-
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
+
